1a9ccd90499c1a675638aac56ae9f9d07cb9325c
Lding <lding0726@163.com>
2019-07-17 16:09:46 +0800

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.default')
+
+@section('title', '关于')
+
+@section('content')
+
+	<h1>关于页</h1>
+
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+
+@section('title', '主页')
+
+@section('content')
+
+	<h1>主页</h1>
+
+@stop
